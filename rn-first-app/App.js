604b53092bcf21ee5a2d03f491b88677d60da667
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-02-21 06:31:25 -0600

    First mobile app

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Change Text"/>    
-
+    <View style={{ paddingTop: 50, height: 200 }}>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Enter your goal"
+          style={{
+            borderColor: "#bbb",
+            borderWidth: 1,
+            width: 350,
+            height: 40
+          }}
+        />
+        <Button title="ADD" />
       </View>
+      <View></View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "red",
+    borderWidth: 1
+  }
 });
