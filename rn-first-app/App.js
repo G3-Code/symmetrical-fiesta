49870ab067181bf618ed526a789645e6ad20f598
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-02-22 13:25:04 -0600

    Added the add button

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState();
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const inputTextHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addButtonHandler = () => {
+    console.log(enteredGoal);
+    setCourseGoals(enteredGoal => [...courseGoals, enteredGoal]);
+    console.log(enteredGoal);
+    console.log("============================" + courseGoals);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.container}>
         <TextInput
           placeholder="Enter your goal"
           style={styles.goalsTextInput}
+          onChangeText={inputTextHandler}
+          value={enteredGoal}
         />
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addButtonHandler} />
       </View>
-      <View></View>
+      <View>
+        <Text>Course Goals</Text>
+      </View>
     </View>
   );
 }
